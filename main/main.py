c7542a57e833d5ccfdef6109ac43593a78f51404
eddex <eddex@outlook.com>
2019-05-29 19:16:32 +0200

    make resizing anchors less THICC, fix some colors

--- main/main.py
+++ main/main.py
@@ -85,7 +85,7 @@ class dragBBox:
     '''
 
     # Size of resizing anchors (depends on LINE_THICKNESS)
-    sRA = LINE_THICKNESS * 2
+    sRA = LINE_THICKNESS
 
     # Object being dragged
     selected_object = None
@@ -953,7 +953,7 @@ if __name__ == '__main__':
     # The colors are in BGR order because we're using OpenCV
     class_rgb = [
         (0, 0, 255), (255, 0, 0), (0, 255, 0), (255, 255, 0), (0, 255, 255),
-        (255, 0, 255), (192, 192, 192), (128, 128, 128), (128, 0, 0),
+        (255, 0, 255), (128, 0, 255), (0, 255, 128), (128, 0, 0),
         (128, 128, 0), (0, 128, 0), (128, 0, 128), (0, 128, 128), (0, 0, 128)]
     class_rgb = np.array(class_rgb)
     # If there are still more classes, add new colors randomly
