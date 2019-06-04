12f662cb5dbe74c2ee875958f19f03fdc6d8abc9
eddex <eddex@outlook.com>
2019-06-04 19:06:15 +0200

    print labels count as markdown table

--- main/count_labels.py
+++ main/count_labels.py
@@ -42,9 +42,26 @@ def read_file(file_name):
                 CLASS_COUNT[class_index] += 1
 
 
-def print_classes():
+def print_classes_md_table():
+    total = 0
     for i in CLASS_MAPPING:
-        print("{}: {}".format(CLASS_MAPPING[i], CLASS_COUNT[i]))
+        total += CLASS_COUNT[i]
+
+    header_row = "class       "
+    header_sep = "-------------"
+    annotations_row = "annotations "
+    distribution_row = "distribution"
+    for i in CLASS_MAPPING:
+        header_row = "{} | {}".format(header_row, CLASS_MAPPING[i])
+        header_sep = "{}|---".format(header_sep)
+        annotations_row = "{} | {}".format(annotations_row, CLASS_COUNT[i])
+        distribution_row = "{} | {:.1f}%".format(distribution_row, CLASS_COUNT[i]/total*100)
+
+    print("total: {}\n".format(total))
+    print(header_row)
+    print(header_sep)
+    print(annotations_row)
+    print(distribution_row)
 
 
 def start():
@@ -53,7 +70,7 @@ def start():
             read_file(filename)
         else:
             print("Skipping file: {}".format(filename))
-    print_classes()
+    print_classes_md_table()
 
 if __name__ == "__main__":
     start()
